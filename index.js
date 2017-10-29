52c2c1c79dbcdfdac778829c8649b9cad31e9cce
Ryan Do <falcsmike7@gmail.com>
2017-10-29 20:56:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 const driver = {}
 
-function updatedriverWithKeyAndValue(driver, key, value) {
-  return Object.assign({}, driver, {[key]: value})
+function updateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign({}, driver, {[key]: value});
 }
